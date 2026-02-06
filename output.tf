1bbe8f36f1b3b19764eebe6727c1aff921243f32
surya-1025 <surya.nallandula1025@gmail.com>
2026-02-06 12:54:57 +0900

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "applicant_name" {
+  value       = var.name
+  description = "Displaying the name of the candidate."
+}
+
+output "applied_position" {
+  value       = var.position
+  description = "Displaying the target job title."
+}
