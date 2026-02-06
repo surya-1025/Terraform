58d86adff9b5f37be9a73f00f7659d325acefe70
surya-1025 <surya.nallandula1025@gmail.com>
2026-02-06 12:55:22 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  type        = string
+  description = "The name of the applicant"
+  default     = "Surya Nallandula"
+}
+
+variable "position" {
+  type        = string
+  description = "The job title being applied for"
+  default     = "Senior DevOps Engineer"
+}
